fed782e9b67bcd63abf0582fafecccb83393803d
Weilinran20001 <95006709+Weilinran20001@users.noreply.github.com>
2024-07-18 11:43:02 +0800

    july18th update2

--- main.py
+++ main.py
@@ -1,136 +1,108 @@
 import numpy as np
 import random
-
+from network import TsodyksHopfieldNetwork 
+from utils import graph, graph_Wills_Leutgeb, hamming_distance, generate_data
 import matplotlib.pyplot as plt
+from scipy.spatial import distance
+from typing import Optional, Union
 
-from networks import HopfieldNetwork, NoveltyHebbianLearningHopfieldNetwork
-from utils import (
-    generate_data, 
-    hamming_distance, 
-)
+def test_FullMorph(num_neurons, num_morphs, pattern, num_repetition):
+    '''
+    30-morph sequence with increasing correlation 
 
+    Input: 
+    num_neurons: number of neurons 
+    num_morphs: number of morphing patterns 
+    pattern: pattern sequence with the first element source pattern; last element target pattern; in-between morphing pattern 
+    num_repetition: how many times the system is given the pattern 
 
-def test_classical_HN():
-    x = np.array([
-        [-1, -1, -1, -1, 1, 1, 1, 1, -1, -1, -1, -1, 1, 1, 1, 1], 
-        [-1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1,], 
-    ])
-    
-    network = HopfieldNetwork(16, synchronous=False, num_iter=20)
-    
-    network.train_weights(x)
-    
-    test_x = np.array([
-        [-1, -1, -1, 1, 1, 1, 1, 1, -1, -1, -1, -1, 1, 1, 1, 1], 
-        [-1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1,], 
-    ])
-    
-    predict_x = network.predict(test_x)
-    
-    return network
+    Return 
+    correlations_gradual: correlation for gradual increasing order  
+    correlations_random: correlation for mixed order 
+    num_morphs: number of morphs 
 
+    '''
+    x0 = pattern[0]
+    x1 = pattern[-1]
+    x_morph = pattern[1:-1]
+    num_seed = 10 
+    eta = 0.5
 
-def test_novelty_HN(target_initial_correlation: float=0.0):
-    num_seeds = 10
-    
-    num_neurons = 80
-    num_morphs = 30
-    
-    X = generate_data(num_neurons, num_morphs, target_initial_correlation=target_initial_correlation)
-    x0 = X[0]
-    x1 = X[-1]
-    x_morph = X[1:-1]
-    
     x_init = np.array([x0, x1])
-    init_correlations = np.sum(x0 * x1) / num_neurons
-
     w_init = np.array([1., 1.])
     hamming_distance_init = hamming_distance(x0, x1)
     
-    eta = 0.5
+    correlations_gradual = np.zeros((num_seed, num_morphs))
+    correlations_random = np.zeros((num_seed, num_morphs))
     
-    correlations_gradual = np.zeros((num_seeds, num_morphs))
-    correlations_random = np.zeros((num_seeds, num_morphs))
-    
-    # gradual morphing
-    for i in range(num_seeds):
-        
-        network = NoveltyHebbianLearningHopfieldNetwork(num_neurons, synchronous=True, num_iter=20)
-        network.train_weights(x_init, w=w_init) # pre-training of two contexts
+    ########################## gradual morphing ##########################
+    for i in range(num_seed): 
+        network = TsodyksHopfieldNetwork(num_neurons, num_iter=10, eta=eta, threshold=0.0)
+        network.train_weights(x=x_init, w= w_init) # pre-training of two contexts
         w_morph = np.zeros((num_morphs, ))
-        
+
         for j in range(num_morphs):
-            x_one_step = network.one_step_dynamics(x_morph[j])
-            hamming_distance_temp = hamming_distance(x_one_step, x_morph[j]) / hamming_distance_init
-            w_morph[j] += eta * hamming_distance_temp
-            network.train_weights(x_morph[j][None], w=w_morph[[j]])
+            for k in range (num_repetition):
+                # if j == len(x_morph)-1: #double check if the last 
+                #     print(np.sum(np.dot(x0,x_morph[j]))/len(x0))
+                x_one_step = network.one_step_dynamics(x_morph[j])
+                hamming_distance_temp = hamming_distance(x_one_step, x_morph[j])/hamming_distance_init
+                w_morph[j] += eta * hamming_distance_temp
+                network.train_weights(x_morph[j][None], w=w_morph[[j]])
             
-            converged_state, _ = network.forward(x_morph[j])
-            correlations_gradual[i, j] = np.sum(converged_state * x_init[0]) / num_neurons
-            
-    # random morphing
-    for i in range(num_seeds):
-        
-        network = NoveltyHebbianLearningHopfieldNetwork(num_neurons, synchronous=True, num_iter=20)
-        network.train_weights(x_init, w=w_init) # pre-training of two contexts
+            # correlation
+            converged_state = network.converge(x_morph[j])
+            correlations_gradual[i, j] = np.sum(converged_state[0] * x_init[0]) / num_neurons
+
+
+    ########################### random morphing ##########################
+    for i in range(num_seed):
+        network = TsodyksHopfieldNetwork(num_neurons, num_iter=10, eta=eta, threshold=0.0)
+        network.train_weights(x=x_init, w=w_init)     
         w_morph = np.zeros((num_morphs, ))
-        
+
         random_perms = np.random.permutation(np.arange(num_morphs))
+        invert_permute = {i: j for i, j in enumerate(random_perms)}
         for j in range(num_morphs):
-            x_one_step = network.one_step_dynamics(x_morph[random_perms[j]])
-            hamming_distance_temp = hamming_distance(x_one_step, x_morph[random_perms[j]]) / hamming_distance_init
-            w_morph[random_perms[j]] += eta * hamming_distance_temp
-            network.train_weights(x_morph[random_perms[j]][None], w=w_morph[[random_perms[j]]])
-            
-            converged_state, _ = network.forward(x_morph[random_perms[j]])
-            correlations_random[i, random_perms[j]] = np.sum(converged_state * x_init[0]) / num_neurons
-            
-    fig, ax = plt.subplots(1, 1, figsize=(5, 5))
-    
-    ax.plot(np.arange(1, num_morphs+1)/num_morphs, correlations_gradual.mean(0), "x--", color="blue", label="gradual morphing")
-    ax.fill_between(
-        np.arange(1, num_morphs+1)/num_morphs, 
-        correlations_gradual.mean(0)-correlations_gradual.std(0), 
-        correlations_gradual.mean(0)+correlations_gradual.std(0), 
-        color="blue", 
-        alpha=0.3, 
-    )
-    
-    ax.plot(np.arange(1, num_morphs+1)/num_morphs, correlations_random.mean(0), "x--", color="green", label="random morphing")
-    ax.fill_between(
-        np.arange(1, num_morphs+1)/num_morphs, 
-        correlations_random.mean(0)-correlations_random.std(0), 
-        correlations_random.mean(0)+correlations_random.std(0), 
-        color="green", 
-        alpha=0.3, 
-    )
-    
-    ax.legend()
-    ax.set_title(f"Initial correlation: {init_correlations:.2f}")
-    ax.set_xlabel("Morphing index")
-    ax.set_ylabel("Correlation")
-    
-    fig.savefig(f"figures/novelty_HN_corr{init_correlations:.2f}.png")
-    
-    return init_correlations, correlations_gradual, correlations_random
+            for k in range (num_repetition):
+                x_one_step = network.one_step_dynamics(x_morph[random_perms[j]])
+                hamming_distance_temp = hamming_distance(x_one_step, x_morph[random_perms[j]]) / hamming_distance_init
+                w_morph[random_perms[j]] += eta * hamming_distance_temp
+                network.train_weights(x_morph[random_perms[j]][None], w=w_morph[[random_perms[j]]])
 
+            converged_state = network.converge(x_morph[random_perms[j]])
+            correlations_random[i, invert_permute[j]] = np.sum(converged_state[0] * x_init[0]) / num_neurons 
+        
+    
+    return correlations_gradual, correlations_random, num_morphs
 
-def test_novelty_HN_wills(target_initial_correlation: float=0.0):
+def test_novelty_HN_wills(X, num_neurons, num_morphs, num_repetitions, memory=False): # Random Morphing 
+    '''
+    Reproduces Wills paper (random morphing) with 6 moprhing patterns
+
+    Input: 
+    X: pattern sequence with the first element source pattern; last element target pattern; in-between morphing pattern
+    num_neurons: number of neurons 
+    num_morphs: number of morphing patterns 
+    num_repetitions:  how many times the system is given the pattern
+    memory: if true: append the source pattern at the end of the morphing sequence 
+
+    Return: 
+    correlations: morphing sequences' correlations with the source pattern  
+    morphs_inds: morphing indices (used for graphing later)
+    '''
     num_seeds = 10
-    
-    num_neurons = 80
-    num_morphs = 30
-    
-    morphs_inds = np.array([12, 24, 6, 18], dtype=int) - 1
-    num_repetitions = 1
-    
-    X = generate_data(num_neurons, num_morphs, target_initial_correlation=target_initial_correlation)
+    if memory == True:
+        morphs_inds = np.array([20, 12, 24, 6, 18, 9, 1], dtype=int) - 1
+    else: 
+        morphs_inds = np.array([20, 12, 24, 6, 18, 9], dtype=int) - 1
+
     x0 = X[0]
     x1 = X[-1]
     x_morph = X[1:-1]
     
     x_init = np.array([x0, x1])
-    init_correlations = np.sum(x0 * x1) / num_neurons
+    # init_correlations = np.sum(x0 * x1) / num_neurons
 
     w_init = np.array([1., 1.])
     hamming_distance_init = hamming_distance(x0, x1)
@@ -138,59 +110,52 @@ def test_novelty_HN_wills(target_initial_correlation: float=0.0):
     eta = 0.5
     
     correlations = np.zeros((num_seeds, num_morphs))
-    
+
     for i in range(num_seeds):
-        network = NoveltyHebbianLearningHopfieldNetwork(num_neurons, synchronous=True, num_iter=20)
+        network = TsodyksHopfieldNetwork(num_neurons, num_iter=10, eta=eta, threshold=0.0)
         network.train_weights(x_init, w=w_init) # pre-training of two contexts
         w_morph = np.zeros((num_morphs, ))
         
         for j in range(len(morphs_inds)):
-            for k in range(num_repetitions):
+            for k in range(num_repetitions): # different sessions: how many times it give the system to learn  
                 x_one_step = network.one_step_dynamics(x_morph[morphs_inds[j]])
                 hamming_distance_temp = hamming_distance(x_one_step, x_morph[morphs_inds[j]]) / hamming_distance_init
                 w_morph[morphs_inds[j]] += eta * hamming_distance_temp
                 network.train_weights(x_morph[morphs_inds[j]][None], w=w_morph[[morphs_inds[j]]])
                 
-            converged_state, _ = network.forward(x_morph[morphs_inds[j]])
+            converged_state, _ = network.converge(x_morph[morphs_inds[j]])
             correlations[i, morphs_inds[j]] = np.sum(converged_state * x_init[0]) / num_neurons
     
-    fig, ax = plt.subplots(1, 1, figsize=(5, 5))
-    
-    correlations = correlations[:, [5, 11, 17, 23]]
-    
-    ax.plot(np.arange(1, len(morphs_inds)+1)/(len(morphs_inds)), correlations.mean(0), "x--", color="green", label="random morphing")
-    ax.fill_between(
-        np.arange(1, len(morphs_inds)+1)/(len(morphs_inds)), 
-        correlations.mean(0)-correlations.std(0), 
-        correlations.mean(0)+correlations.std(0), 
-        color="green", 
-        alpha=0.3, 
-    )
-    
-    ax.legend()
-    ax.set_title(f"Wills Experiments (Initial correlation: {init_correlations:.2f})")
-    ax.set_xlabel("Morphing index")
-    ax.set_ylabel("Correlation")
-    
-    fig.savefig(f"figures/novelty_HN_corr{init_correlations:.2f}_wills.png")
+    return correlations, morphs_inds
 
+def test_novelty_HN_leutgeb(X, num_neurons, num_morphs, num_repetitions, memory=False): # Gradual Morphing 
+    '''
+    Reproduces Leutgeb paper (gradual morphing) with 6 moprhing patterns
 
-def test_novelty_HN_leutgeb(target_initial_correlation: float=0.0):
+    Input: 
+    X: pattern sequence with the first element source pattern; last element target pattern; in-between morphing pattern
+    num_neurons: number of neurons 
+    num_morphs: number of morphing patterns 
+    num_repetitions:  how many times the system is given the pattern
+    memory: if true: append the source pattern at the end of the morphing sequence 
+
+    Return: 
+    correlations: morphing sequences' correlations with the source pattern  
+    morphs_inds: morphing indices (used for graphing later)
+    '''
+    
     num_seeds = 10
-    
-    num_neurons = 80
-    num_morphs = 30
-    
-    morphs_inds = np.array([6, 12, 18, 24], dtype=int) - 1
-    num_repetitions = 2
-    
-    X = generate_data(num_neurons, num_morphs, target_initial_correlation=target_initial_correlation)
+    if memory == True:
+        morphs_inds = np.array([6, 9, 12, 18, 20, 24, 1], dtype=int) - 1
+    else: 
+        morphs_inds = np.array([6, 9, 12, 18, 20, 24], dtype=int) - 1
+
     x0 = X[0]
     x1 = X[-1]
     x_morph = X[1:-1]
     
     x_init = np.array([x0, x1])
-    init_correlations = np.sum(x0 * x1) / num_neurons
+    # init_correlations = np.sum(x0 * x1) / num_neurons
 
     w_init = np.array([1., 1.])
     hamming_distance_init = hamming_distance(x0, x1)
@@ -198,45 +163,102 @@ def test_novelty_HN_leutgeb(target_initial_correlation: float=0.0):
     eta = 0.5
     
     correlations = np.zeros((num_seeds, num_morphs))
-    
+
     for i in range(num_seeds):
-        network = NoveltyHebbianLearningHopfieldNetwork(num_neurons, synchronous=True, num_iter=20)
+        network = TsodyksHopfieldNetwork(num_neurons, num_iter=10, eta=eta, threshold=0.0)
         network.train_weights(x_init, w=w_init) # pre-training of two contexts
         w_morph = np.zeros((num_morphs, ))
         
         for j in range(len(morphs_inds)):
-            for k in range(num_repetitions):
+            for k in range(num_repetitions): #different sessions: how many times it give the system to learn 
                 x_one_step = network.one_step_dynamics(x_morph[morphs_inds[j]])
                 hamming_distance_temp = hamming_distance(x_one_step, x_morph[morphs_inds[j]]) / hamming_distance_init
                 w_morph[morphs_inds[j]] += eta * hamming_distance_temp
                 network.train_weights(x_morph[morphs_inds[j]][None], w=w_morph[[morphs_inds[j]]])
                 
-            converged_state, _ = network.forward(x_morph[morphs_inds[j]])
+            converged_state, _ = network.converge(x_morph[morphs_inds[j]])
             correlations[i, morphs_inds[j]] = np.sum(converged_state * x_init[0]) / num_neurons
-    
-    fig, ax = plt.subplots(1, 1, figsize=(5, 5))
-    
-    correlations = correlations[:, [5, 11, 17, 23]]
-    
-    ax.plot(np.arange(1, len(morphs_inds)+1)/(len(morphs_inds)), correlations.mean(0), "x--", color="green", label="random morphing")
-    ax.fill_between(
-        np.arange(1, len(morphs_inds)+1)/(len(morphs_inds)), 
-        correlations.mean(0)-correlations.std(0), 
-        correlations.mean(0)+correlations.std(0), 
-        color="green", 
-        alpha=0.3, 
-    )
-    
-    ax.legend()
-    ax.set_title(f"Leutgeb Experiments (Initial correlation: {init_correlations:.2f})")
-    ax.set_xlabel("Morphing index")
-    ax.set_ylabel("Correlation")
-    
-    fig.savefig(f"figures/novelty_HN_corr{init_correlations:.2f}_leutgeb.png")
+    return correlations, morphs_inds
 
-if __name__=="__main__":
-    # network = test_classical_HN()
-    # for corr in np.arange(0, 0.25, 0.05):
-    #     init_correlations, correlations_gradual, correlations_random = test_novelty_HN(corr)
-    # test_novelty_HN_wills()
-    test_novelty_HN_leutgeb()
+def test_memory_interference_exp(num_neurons, num_morphs, pattern, num_repetition): # last pattern is the first pattern again 
+    '''
+    function to produce memory interference for Wills and Leutgeb experiment 
+
+    '''
+
+    x0 = np.expand_dims(pattern[0],0)
+    x1 = np.expand_dims(pattern[-1],0)
+    x_morph = np.concatenate((pattern[1:-1], x0), axis=0)
+    pattern = np.concatenate((x0, x_morph, x1), axis=0)
+    num_morphs = int(len(x_morph))
+    correlations_random, morph_idx = test_novelty_HN_wills(pattern, num_neurons, num_morphs, num_repetition, memory = True)
+    correlations_gradual, morph_idx = test_novelty_HN_leutgeb(pattern, num_neurons, num_morphs, num_repetition, memory = True)
+        
+    return correlations_gradual, correlations_random, morph_idx
+
+def test_memory_interference_morph30(num_neurons, num_morphs, pattern, num_repetition):
+    '''
+    produce memory interference for 30 morphing sequence 
+
+    '''
+    x0 = np.expand_dims(pattern[0],0)
+    x1 = np.expand_dims(pattern[-1],0)
+    x_morph = np.concatenate((pattern[1:-1], x0), axis=0) # append the source pattern at the end of x_morph
+    pattern = np.concatenate((x0, x_morph, x1), axis=0)
+    num_morphs = int(len(x_morph))
+    correlations_gradual, correlations_random, num_morph = test_FullMorph(num_neurons, num_morphs, pattern, num_repetition)
+    return correlations_gradual, correlations_random, num_morph
+
+'''
+Master function to produce all results 
+
+Input: 
+method: options: 'test_FullMorph'// 'test_memory_interference_morph30' // 'test_novelty_HN_wills'//  'test_novelty_HN_leutgeb'// 'test_memory_interference_exp'
+num_neurons: number of neurons 
+num_morphs: number of morphing 
+t_s_corrr_list: target-source correlation list (a list from 0.0 to 1.0)
+num_repetitions: number of repetitions
+
+Return: 
+'test_FullMorph' and 'test_memory_interference_morph30' ------ None 
+'test_novelty_HN_wills' and 'test_novelty_HN_leutgeb' ------ correlation for each morphing pattern + morph_idx 
+'test_memory_interference_exp' ------ gradual_increasing correlation; mixed_increasing correlation; morph_idx
+'''
+def main (method, num_neurons, num_morphs, t_s_corr_list, num_repetitions: Optional[int]=None):     
+    if method.__name__ == 'test_FullMorph' or method.__name__ == 'test_memory_interference_morph30':
+            average_corr_gradual = []
+            average_corr_random = []
+            for t_s_corr in t_s_corr_list: # Different Correlation 
+                print('correlation between the source and the target is: ', t_s_corr)
+                for _ in range (1): # Same Correlation, Different Pattern
+                    pattern = generate_data(num_neurons, num_morphs, target_initial_correlation=t_s_corr)
+                    correlations_gradual, correlations_random, morph_idx = method(num_neurons, num_morphs, pattern, num_repetitions) # [num_seed, num_morph]
+                    average_corr_gradual.append(correlations_gradual)
+                    average_corr_random.append(correlations_random)
+                graph(method=method, average_corr_gradual = np.stack(average_corr_gradual,0).mean(0), average_corr_random= np.stack(average_corr_random,0).mean(0), num_morphs= morph_idx, t_s_corr = t_s_corr)
+
+    elif method.__name__ == 'test_novelty_HN_wills' or method.__name__ == 'test_novelty_HN_leutgeb':
+        pattern_corr = []
+        for t_s_corr in t_s_corr_list: # Different Correlation
+            average_corr = [] # if Same Correlation, Different Pattern is needed
+            print('correlation between the source and the target is: ', t_s_corr)
+            for _ in range (1): # Same Correlation, Different Pattern
+                X = generate_data(num_neurons, num_morphs, target_initial_correlation=t_s_corr)
+                correlation, morph_idx = method(X, num_neurons, num_morphs, num_repetitions)
+                average_corr.append(correlation) #[num_seed, num_morph]
+            pattern_corr.append(np.stack(average_corr,0).mean(0))
+        return pattern_corr, morph_idx
+    
+    elif method.__name__ == 'test_memory_interference_exp':
+        average_corr_gradual = []
+        average_corr_random = []
+        for t_s_corr in t_s_corr_list: # Different Correlation 
+            print('correlation between the source and the target is: ', t_s_corr)
+            for _ in range (1): # Same Correlation, Different Pattern
+                pattern = generate_data(num_neurons, num_morphs, target_initial_correlation=t_s_corr)
+                correlations_gradual, correlations_random, morph_idx = method(num_neurons, num_morphs, pattern, num_repetitions) # [num_seed, num_morph]
+                average_corr_gradual.append(correlations_gradual)
+                average_corr_random.append(correlations_random)
+        return  np.stack(average_corr_gradual,0), np.stack(average_corr_random,0), morph_idx
+
+
--- utils.py
+++ utils.py
@@ -1,6 +1,13 @@
-import numpy as np
+import numpy as np 
+import matplotlib.pyplot as plt 
+from typing import Optional, Union
+import matplotlib.colors as mcolors
 import random
+from typing import Optional, Union
 
+def hamming_distance(x1: np.ndarray, x2: np.ndarray):
+    assert len(x1) == len(x2)
+    return (len(x1) - np.sum(np.equal(x1, x2)))/len(x1)
 
 def generate_data(
     num_neurons: int, 
@@ -13,7 +20,6 @@ def generate_data(
     x1 = x0.copy()
     flip_indices = np.random.choice(num_neurons, num_flips, replace=False)
     x1[flip_indices] *= -1
-    
     X = np.zeros((num_morphs+2, num_neurons))
     X[0] = x0
     X[-1] = x1
@@ -27,10 +33,113 @@ def generate_data(
         x_morph_temp = X[i].copy()
         x_morph_temp[morph_inds[i]] = -x_morph_temp[morph_inds[i]]
         X[i+1] = x_morph_temp
-    
     return X
 
 
-def hamming_distance(x1: np.ndarray, x2: np.ndarray):
-    assert len(x1) == len(x2)
-    return len(x1) - np.sum(np.equal(x1, x2))
+####################### graphing utils #######################
+css4_colors = mcolors.CSS4_COLORS
+color1 = '#507b9c' #grad
+color2 = '#feb308' #rnd
+
+def graph(method, 
+          num_morphs: Optional[int]=None, t_s_corr: Optional[list]=None, average_corr_gradual:Optional[list]=None, average_corr_random:Optional[list]=None,
+          correlations: Optional[list]=None, morphs_inds: Optional[list]=None):
+    
+    if method.__name__ == 'test_FullMorph' or method.__name__ == 'test_memory_interference_morph30':
+        if method.__name__ == 'test_FullMorph': 
+            path = '/Users/weilinran/Desktop/CCSNN/tsodyks_novelty_HN_neuron_2006/fig/Morph30/' + 'Morph30_{}.jpg'.format(np.round(t_s_corr,3))
+        else: 
+            path = '/Users/weilinran/Desktop/CCSNN/tsodyks_novelty_HN_neuron_2006/fig/memory_interference/Morph30/' +'memory30_{}.jpg'.format(np.round(t_s_corr,3))
+
+        fig, ax = plt.subplots(1, 1, figsize=(8, 5))
+        ax.plot(average_corr_gradual.mean(0),'-.r', c=color1, label='Gradual Morphing', linewidth=3)
+        ax.fill_between(
+            np.arange(1, num_morphs+1), 
+            average_corr_gradual.mean(0) - average_corr_gradual.std(0), 
+            average_corr_gradual.mean(0) + average_corr_gradual.std(0), 
+            color=color1, 
+            alpha=0.3, 
+        )
+        ax.plot(average_corr_random.mean(0), color=color2, label='Random Morphing', linewidth=3)
+        ax.fill_between(
+            np.arange(1, num_morphs+1), 
+            average_corr_random.mean(0)- average_corr_random.std(0), 
+            average_corr_random.mean(0)+ average_corr_random.std(0), 
+            color=color2, 
+            alpha=0.3, 
+        )
+
+        ax.set_ylim(-0.2,1.2)
+        ax.set_ylabel('Correlation with The First Pattern')
+        ax.set_xlabel('Morphing Index')
+        fig.suptitle('Morphing Patterns with Correlation: {}'.format(np.round(t_s_corr,3)))
+        fig.legend()
+        plt.show()
+        fig.savefig(path)
+
+    elif method.__name__ == 'test_novelty_HN_wills' or method.__name__ == 'test_novelty_HN_leutgeb':
+        fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+        correlations = np.stack(correlations, 0).mean(0)
+        correlations = correlations[[5, 8, 11, 17, 19, 23]]
+        if method.__name__== 'test_novelty_HN_wills':
+            name = 'Wills'
+            label = 'Random Morphing'
+        else: 
+            name = 'Leutgeb'
+            label = 'Gradual Morphing'
+
+        ax.plot(np.arange(1, len(morphs_inds)+1), correlations, color=color1, label=label, linewidth=3)
+        ax.set_ylim(-0.2,1.2)
+
+        ax.legend()
+        ax.set_title('{} Experiments (Initial correlation_{})'.format(name, np.round(t_s_corr,3)))
+        ax.set_xlabel("Morphing index")
+        ax.set_ylabel("Correlation with The First Pattern")
+        
+        fig.savefig('/Users/weilinran/Desktop/CCSNN/tsodyks_novelty_HN_neuron_2006/fig/'+ name + '/novelty_HN_corr{}.jpg'.format(np.round(t_s_corr,3)))
+        plt.show()
+
+def graph_Wills_Leutgeb(corr_wills, corr_leut, morph_idx, t_s_corr, memory=False):
+    if memory == False:
+        path = '/Users/weilinran/Desktop/CCSNN/tsodyks_novelty_HN_neuron_2006/fig/Will_Leutgeb/' + 'Will_Leut_memory_{}.jpg'.format(np.round(t_s_corr,3))
+    else: 
+        path = '/Users/weilinran/Desktop/CCSNN/tsodyks_novelty_HN_neuron_2006/fig/memory_interference/Wills_Leutgeb/' + 'Will_Leut_memory_{}.jpg'.format(np.round(t_s_corr,3))
+
+    fig, ax = plt.subplots(1, 1, figsize=(8, 5))
+
+    # Wills experiment
+    if memory == True:
+        corr_wills = corr_wills[:,[5, 8, 11, 17, 19, 23, 0]]
+    else: 
+        corr_wills = corr_wills[:,[5, 8, 11, 17, 19, 23]]
+
+    # Leutgeb experiment 
+    if memory == True:
+        corr_leut = corr_leut[:,[5, 8, 11, 17, 19, 23, 0]]
+    else: 
+        corr_leut = corr_leut[:,[5, 8, 11, 17, 19, 23]]
+
+    ax.plot(np.arange(1, len(morph_idx)+1), corr_leut.mean(0),'-.r', color=color1, label='Gradual Morphing', linewidth=3)
+    ax.plot(np.arange(1, len(morph_idx)+1), corr_wills.mean(0),color=color2, label='Random Morphing', linewidth=3)
+    
+    ax.fill_between(
+            np.arange(1, len(morph_idx)+1), 
+            corr_wills.mean(0)-corr_wills.std(0), 
+            corr_wills.mean(0)+corr_wills.std(0), 
+            color=color1, 
+            alpha=0.3, )
+    
+    ax.fill_between(
+            np.arange(1, len(morph_idx)+1), 
+            corr_leut.mean(0)-corr_leut.std(0), 
+            corr_leut.mean(0)+corr_leut.std(0), 
+            color=color2, 
+            alpha=0.3, )
+
+    ax.set_ylim(-0.2,1.2)
+    ax.set_ylabel('Correlation with The First Pattern')
+    ax.set_xlabel('Morphing Index')
+    fig.suptitle('Morphing Patterns with Correlation: {}'.format(np.round(t_s_corr,3)))
+    fig.legend()
+    plt.show()
+    fig.savefig(path)
